b4c2978986fbdf3f6c57e214799272360e6d7094
Aleksey <aleksey.ukrnets@mail.ru>
2022-01-11 23:39:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination"
+
+import "./styles.css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Pagination
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+
+
+export default function App() {
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="wrapper">
+    <>
+    
+    <Swiper style={ {'--swiper-pagination-color': 'rgb(206, 71, 154)'} } slidesPerView={1} spaceBetween={10} pagination={{
+  "clickable": true
+}} breakpoints={{
+  "540": {
+    "slidesPerView": 2,
+    "spaceBetween": 20
+  },
+  "768": {
+    "slidesPerView": 4,
+    "spaceBetween": 40
+  },
+  "1024": {
+    "slidesPerView": 5,
+    "spaceBetween": 50
+  }
+}} className="mySwiper">
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  <SwiperSlide  className="swipe"></SwiperSlide>
+  </Swiper>
+    </>
+  </div>
+  )
 }
-
-export default App;
